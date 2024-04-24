a6e0611514f00f2bb4a0f1c8db0bb7622a9d8139
kundan-2508 <kun123658@gmail.com>
2024-04-24 11:53:19 +0530

    Added content for Day6

---
+++ Day6/index.js
@@ -0,0 +1,37 @@
+const express =  require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, resp) =>{
+    resp.send("Working")
+})
+
+app.get("/wheather", (req, resp) =>{
+    // console.log(req.query)
+    // console.log(req.query.city)
+    const { city } = req.query
+    resp.send("Wheather is fine in " + city)
+})
+
+app.get("/students/:id", (req, resp)=>{
+    const id = req.params.id;
+    resp.send("Id of students is " + id)
+})
+
+app.listen(4500, (req, resp)=>{
+    console.log("Server is running at port 4500")
+})
+
+// what is database
+// we can store data in many format like file, etc.
+// then why database ?
+// sql & nosql
+// SQL - structured query language
+// NoSQL - not a structured query language
+// which one is more flexible ? - nosql
+// example in sql
+// some of the examples of SQL are mySQL, postgres, MSSql, Oracle
+// some of the example of NoSql are mongoDb, Cassandra
+// NoSQL is more flexible
---
+++ Day6/queryAndParam.js
@@ -0,0 +1,5 @@
+// query - something which is required from 
+// server - dynamic response
+// wheather api => www.wheatherapi.com?city=pune
+// here city = pune is query
+// params - students/2
